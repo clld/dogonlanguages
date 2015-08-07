bf612d91c37d8894f1d0e7719fe2ea85a1e8e628
xrotwang <xrotwang@googlemail.com>
2015-08-07 12:28:03 +0200

    upgrade clld

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 requires = [
-    'clld>=1.2.1',
-    'clldmpg>=0.8',
+    'clld>=1.5.0',
+    'clldmpg>=1.0.0',
     'psycopg2',
     'fuzzywuzzy',
 ]
